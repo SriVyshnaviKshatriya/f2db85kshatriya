9418355592a7f1dc6fd92aa10b9d5e990c155fec
Sri Vyshnavi Kshatriya <vyshnavisri1908@gmail.com>
2022-11-28 21:37:47 -0600

    Added secured to create and delete

--- routes/employee.js
+++ routes/employee.js
@@ -2,11 +2,18 @@ var express = require('express');
 const employee = require('../controllers/employee');
 var router = express.Router();
 
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+}
 /* GET home page. */
 router.get('/', employee.employee_view_all_Page);
 router.get('/detail', employee.employee_view_one_Page);
-router.get('/create', employee.employee_create_Page);
-router.get('/update', employee.employee_update_Page);
-router.get('/delete', employee.employee_delete_Page);
+router.get('/create',secured, employee.employee_create_Page);
+router.get('/update',secured, employee.employee_update_Page);
+router.get('/delete',secured, employee.employee_delete_Page);
 
 module.exports = router;
